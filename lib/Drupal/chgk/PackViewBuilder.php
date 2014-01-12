203bcfe9e157ddbe38d283e4235ef6aff6d688e6
Database account <chgk-db@db.chgk.info>
2014-01-13 00:05:58 +0400

    view controller for pack

---
+++ lib/Drupal/chgk/PackViewBuilder.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\chgk\PackViewBuilder.
+ */
+
+namespace Drupal\chgk;
+
+use Drupal\Core\Entity\EntityViewBuilder;
+
+/**
+ * Render controller for nodes.
+ */
+class PackViewBuilder extends EntityViewBuilder {
+
+}
